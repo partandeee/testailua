069a2a9343a0517ca5270de7907ed7b67c484008
partandeee <112544164+partandeee@users.noreply.github.com>
2023-01-25 20:11:46 +0200

    Delete string.js

--- string.js
+++
@@ -1,6 +0,0 @@
-process.stdout.write("Enter something : ");
-
-process.stdin.on("data", function(inputFromUser){
-    process.stdout.write("User entered : " + inputFromUser.toString().trim().toUpperCase() + " it shall end now");
-    process.exit();
-});
